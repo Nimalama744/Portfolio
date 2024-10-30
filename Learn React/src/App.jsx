b827d40b6c2d5777966676273465aa42a9aa4dbb
Nima Lama <164026206+Nimalama744@users.noreply.github.com>
2024-10-31 03:02:29 +0545

    changes i guess

---
+++ Learn React/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-blue-600 bg-slate-700'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
--- drumkit.js
+++ drumkit.js
@@ -1,8 +1,22 @@
 
 
 
-function play(){
-
-    var audio = new audio ('https://www.soundsnap.com/attack_openhat16_wav');
+window.addEventListener('keyup', function(e) {
+    
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const drum = document.querySelector(`.drum[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
     audio.play();
+    console.log(drum);
+    drum.classList.add('playing');
+    
+
+});
+
+function removeTransition(){
+    drum.classList.remove('playing');
 }
+
+const keys = document.querySelectorAll('.drum');
+keys.forEach (key => key.add('transitionend',removeTransition));
